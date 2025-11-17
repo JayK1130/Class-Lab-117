8ea88ee2037f62ec92fb176dece17c7952ca4db8
“Jay <jaykim1130@icloud.com>
2025-11-17 12:46:17 -0800

    117

---
+++ main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+int treeHeight(Node *root) {
+    if (root == nullptr) return -1;
+    if (root->left == nullptr && root->right == nullptr) return 0;
+    return 1 + max(treeHeight(root->left), treeHeight(root->right));
+}
+
+void printLevel(Node* root, int level, vector<int>& saved) {
+    if (root == nullptr) return;
+    if (level == 0) {
+        cout << root->data << " ";
+        saved.push_back(root->data);
+    } else {
+        printLevel(root->left, level - 1, saved);
+        printLevel(root->right, level - 1, saved);
+    }
+}
+
+void printLevelOrder(Node *root, vector<int>& saved) {
+    int h = treeHeight(root);
+    for (int i = 0; i <= h; i++) {
+        printLevel(root, i, saved);
+        cout << endl;
+    }
+}
+
+Node *constructCBT(vector<int> &tree_values) {
+    Node *root = new Node(tree_values[0]);
+    queue<Node*> q;
+    q.push(root);
+    long unsigned int i = 1;
+
+    while (!q.empty() && i < tree_values.size()) {
+        Node *current = q.front();
+        q.pop();
+
+        if (i < tree_values.size()) {
+            current->left = new Node(tree_values[i]);
+            q.push(current->left);
+            i++;
+        }
+        if (i < tree_values.size()) {
+            current->right = new Node(tree_values[i]);
+            q.push(current->right);
+            i++;
+        }
+    }
+
+    return root;
+}
+
+Node *constructBST(Node *root, int value) {
+    if (root == nullptr) return new Node(value);
+    if (value < root->data)
+        root->left = constructBST(root->left, value);
+    else
+        root->right = constructBST(root->right, value);
+    return root;
+}
+
+int main() {
+    vector<int> tree_values = {10, 15, 20, 3, 5, 30, 17};
+    vector<int> saved;
+    Node *root = constructCBT(tree_values);
+
+    printLevelOrder(root, saved);
+    cout << endl;
+
+    vector<int> tree_values2 = {4, 2, 1, 9, 6, 7, 8};
+    saved.clear();
+    root = nullptr;
+
+    for (auto element : tree_values2)
+        root = constructBST(root, element);
+
+    printLevelOrder(root, saved);
+    cout << endl;
+
+    return 0;
+}
